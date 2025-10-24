5b531bfb27da85f385b975c3f4e476043d7b31f4
Ricardo Farias <ricardofariasg4@gmail.com>
2025-10-23 22:10:44 -0300

    fix: ajusta os providers da classe auth para usar o modelo de Usuario corretamente

--- config/auth.php
+++ config/auth.php
@@ -15,7 +15,7 @@ return [
 
     'defaults' => [
         'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+        'passwords' => env('AUTH_PASSWORD_BROKER', 'USUARIOS'),
     ],
 
     /*
@@ -91,8 +91,8 @@ return [
     */
 
     'passwords' => [
-        'users' => [
-            'provider' => 'users',
+        'USUARIOS' => [
+            'provider' => 'USUARIOS',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
