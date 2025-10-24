2b05b6519472d1bec1c15a2c3d0a771b0cde9d42
Ricardo Farias <ricardofariasg4@gmail.com>
2025-10-23 22:11:58 -0300

    fix: remocao do HasApiTokens para migracao para modelo de sessao

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -6,12 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class Usuario extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UsuarioFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable;
 
     protected $table = 'USUARIOS';
     protected $primaryKey = 'id_usuario';
