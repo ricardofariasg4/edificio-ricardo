720956ff70fa36d9b0fa410c5c7aef0650a3ae8a
Ricardo Farias <ricardo.farias@rentcars.com>
2025-11-13 22:04:40 -0300

    inicia implementacao do middleware

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Middleware\GaranteCadastroPorAutorizado;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
     Route::get('/logout', 'logout')->middleware('auth');
+});
+
+Route::middleware(['auth', GaranteCadastroPorAutorizado::class])->group(function () {
+    Route::controller(UsuarioController::class)->group(function () {
+        Route::get('/users', 'index');
+        Route::post('/user', 'store');
+        Route::get('/user', 'show');
+        Route::put('/user', 'update');
+        Route::delete('/user', 'destroy');
+    });
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/ping', function () {
+    return 'pong';
 });
